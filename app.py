a76e3286198daad2b60cc15e00c84b37e1056b4b
Julia-Susser <you@example.com>
2020-12-30 18:33:05 -0800

    make it better

--- app.py
+++ app.py
@@ -14,49 +14,30 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 db = firebase.database()
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
+@app.route('/', methods=['post', 'get'])
+def hello():
+    dir = list(map(lambda x: x, os.listdir("static/img")))
+    print(dir)
+    dir = {"pics":dir}
+    if request.method == 'POST':
+        val += 1
+        pic = dir[val]
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        val = 0
 
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template('index.html', dir=dir
+    )
+@app.route('/send', methods=['post', 'get'])
+def send():
+    print("here")
+    ratings = request.values.get('ratings')
+    l = json.loads(ratings)
+    t = dict()
+    for x, y in l.items():
+        t[str(x)] = str(y)
+    print(t)
+    db.child("ratings").push(ratings) #replaces appending to events variable with firebase db call.
+    return "done"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
